948231c506f6b5f968e0a53dd3f88ebff371e829
Guest User <Guest@g-04-05-lab.mddn.vuw.ac.nz>
2015-09-21 14:29:23 +1200

    added rigid body and move code to player obj
    this is one way of moving, dont think its what we want thugh.

---
+++ Assets/scripts/playerController.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody pl;
+	// Use this for initialization
+	void Start () {
+		pl = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate(){
+		float moveSideways = Input.GetAxis("Horizontal");
+
+		Vector3 dir = new Vector3 (moveSideways, 0.0f, 0.0f);
+
+		pl.AddForce (dir * speed);
+	}
+
+}
