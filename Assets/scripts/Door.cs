5dc94d80f8b03bba00593831567b955c0066a4ce
Guest User <Guest@h-04-07-lab.mddn.vuw.ac.nz>
2015-10-08 13:56:07 +1300

    update all

---
+++ Assets/scripts/Door.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Door : MonoBehaviour {
+	public Animator anim; 
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter ( Collider other) {
+		print ("hello!");
+		anim.SetBool ("DoorOpen", true);
+		anim.SetBool ("DoorClose", false);
+	}
+
+	void OnTriggerExit (Collider other){
+		anim.SetBool ("DoorOpen", false);
+		anim.SetBool ("DoorClose", true);
+	}
+	
+}
