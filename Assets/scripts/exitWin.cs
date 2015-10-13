d0af0cad9f4794cc4278b7afc1a0b1c3709d3c0f
Guest User <Guest@d-01-06-lab.mddn.vuw.ac.nz>
2015-10-14 12:01:49 +1300

    changed .js files to .cs

---
+++ Assets/scripts/exitWin.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+
+public class exitWin : MonoBehaviour {
+
+	string levelToLoad;
+
+	public void OnTriggerEnter (Collider hit){
+		Application.LoadLevel(levelToLoad);
+	}
+}
---
+++ Assets/scripts/pushing.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pushing : MonoBehaviour {
+
+	float pushPower = 2.0;
+	public void OnControllerColliderHit (ControllerColliderHit hit){
+		Rigidbody body = hit.collider.attachedRigidbody;
+
+		if (body == null || body.isKinematic) 
+			return ;		
+		if (hit.moveDirection.y < -0.3) 
+			return;
+		Vector3 pushDir = Vector3 (hit.moveDirection.x,0,0);
+	
+		body.velocity = pushDir * pushPower;
+	}
+
+}
