ba09d730a0cd1e68f65be7737bbc642c2a9b069e
Guest User <Guest@a-08-08-lab.mddn.vuw.ac.nz>
2015-10-14 19:40:36 +1300

    finalized maybe

---
+++ Assets/scripts/quit.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class quit : MonoBehaviour {
+
+	public void Update() {
+		if (Input.GetMouseButton (0) == true) {
+			Debug.Log ("Pressed left click.");
+			print ("gggdfdf");
+			Application.Quit();
+		}
+	}
+}
