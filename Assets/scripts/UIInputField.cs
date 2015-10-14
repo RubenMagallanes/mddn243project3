0d8f68db4c2dab7bace7ca8c0a6435ac7301922b
Guest User <Guest@a-08-08-lab.mddn.vuw.ac.nz>
2015-10-14 20:14:57 +1300

    final

--- Assets/scripts/UIInputField.cs
+++ Assets/scripts/UIInputField.cs
@@ -18,14 +18,4 @@ public class UIInputField : MonoBehaviour {
 	public void CharacterField(string txt){
 		actionText.text = txt;
 	}
-
-	public void clickTest (){
-		Debug.Log ("clicc");
-		//get text from text box
-		//clear text boc
-		//parse for commands
-		//construct objects and give to robot
-		//set flag to true so robot executes commands
-
-	}
 }
